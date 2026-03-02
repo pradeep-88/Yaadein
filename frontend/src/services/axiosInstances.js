bd268d5f2335174cd45be5ad94150f140af7a450
Pradeep rajput <pradeeprajput.er@gmail.com>
2026-03-02 21:41:03 +0530

    Updating

--- frontend/src/services/axiosInstances.js
+++ frontend/src/services/axiosInstances.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'https://yaadein-hjot.onrender.com/api',
   withCredentials: true,
 })
 
